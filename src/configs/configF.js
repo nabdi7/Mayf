abca6e301e271e9f2e9941aaa84e5aaf47c28267
nabdi7 <36966910+nabdi7@users.noreply.github.com>
2024-11-28 16:53:45 -0800

    updated

--- src/configs/configF.js
+++ src/configs/configF.js
@@ -1,12 +1,12 @@
 
 const configF = {
-  apiKey: "AIzaSyBijONZt_oA0dviWHsOGNpgCojPXhAAfoA",
-  authDomain: "abumsjapp-test.firebaseapp.com",
-  databaseURL: "https://abumsjapp-test.firebaseio.com",
-  projectId: "abumsjapp-test",
-  storageBucket: "abumsjapp-test.appspot.com",
-  messagingSenderId: "579046275467",
-  appId: "1:579046275467:web:6191d614f89bf1b2c3578e"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 module.exports = {configF}
